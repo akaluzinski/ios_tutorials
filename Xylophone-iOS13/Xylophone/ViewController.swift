525cea477648b13c009183a85a78aaf3b41623e7
Adrian <kaluza@gmail.com>
2022-10-17 14:55:21 +0200

    Play C sound on click

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -6,14 +6,38 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
+    @IBAction func keyPressed(_ sender: UIButton) {    
+        playSound()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
     
 
