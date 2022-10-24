ff21ea08bbae785839b00b24ec7e1f14b5534e49
Adrian <kaluza@gmail.com>
2022-10-24 15:17:42 +0200

    Add timer on egg click

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = [
+        "Soft": 300,
+        "Medium": 420,
+        "Hard": 720
+    ]
+    
+    var remainingSeconds = 60
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        let time = eggTimes[hardness]!
+    
+        Timer.scheduledTimer(timeInterval: 1.0,
+                             target: self,
+                             selector: #selector(updateTimer),
+                             userInfo: nil,
+                             repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if remainingSeconds > 0 {
+            print("\(remainingSeconds) seconds.")
+            remainingSeconds -= 1
+        }
+    }
+    
 }
